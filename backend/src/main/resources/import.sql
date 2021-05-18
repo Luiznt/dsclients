0badb6a5fc0d36376f36d0276503823568548a8f
Luiznt <luiznt@hotmail.com>
2021-05-17 23:30:30 -0300

    Entity Client, repository, H2 seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,17 @@
+insert into tb_client(id, name, cpf, income, birth_date, children) values (1,	'Jose Maria',			'12345678901',	100.0,	'2000-01-27T13:00:00Z', 1 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (2,	'Jeruza Santos',		'43254376532',	234.2,	'2005-10-20T14:00:00Z', 2 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (3,	'Marcelo Holanda',		'36985214723',	530.0,	'1990-04-03T15:00:00Z', 0 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (4,	'Anderson da Silva',	'85274196312',	1230.0,	'1998-02-25T03:00:00Z', 3 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (5,	'Pedro Nascimento',		'78945612345',	234.2,	'2001-03-12T16:00:00Z', 1 );
+
+insert into tb_client(id, name, cpf, income, birth_date, children) values (6,	'Jose Santos',			'15948726315',	102.0,	'2000-11-27T13:00:00Z', 1 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (7,	'Jeruza Maria',			'11234456987',	222.2,	'2005-11-20T14:00:00Z', 2 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (8,	'Marcela Braga',		'95487984123',	530.0,	'1990-01-03T15:00:00Z', 0 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (9,	'Ricardo Montezuna',	'59987456154',	123.0,	'1998-02-25T03:00:00Z', 3 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (10,	'João Benedito Costa',	'20136089054',	232.2,	'2002-03-12T16:00:00Z', 1 );
+
+insert into tb_client(id, name, cpf, income, birth_date, children) values (11,	'Jailson Nobrega',		'01234695153',	103.0,	'1993-01-27T13:00:00Z', 1 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (12,	'Cristina Amaral',		'00525448925',	2434.2,	'1995-10-20T14:00:00Z', 2 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (13,	'Elza Soares Costa',	'51459985447',	5330.0,	'1991-04-03T15:00:00Z', 0 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (14,	'Daniela Millano',		'21548775481',	130.0,	'1999-02-25T03:00:00Z', 3 );
+insert into tb_client(id, name, cpf, income, birth_date, children) values (15,	'Davi Henrique Fontes',	'21030206584',	1234.2,	'2000-03-12T16:00:00Z', 1 );
