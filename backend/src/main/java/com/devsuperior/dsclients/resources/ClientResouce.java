01334a41b348a5c032dab95a90e622dcaeea1244
Luiznt <luiznt@hotmail.com>
2021-05-18 15:29:01 -0300

    GET Client paged

--- backend/src/main/java/com/devsuperior/dsclients/resources/ClientResouce.java
+++ backend/src/main/java/com/devsuperior/dsclients/resources/ClientResouce.java
@@ -3,9 +3,13 @@ package com.devsuperior.dsclients.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclients.dto.ClientDTO;
@@ -18,10 +22,26 @@ public class ClientResouce {
 	@Autowired
 	private ClientService service;
 	
+	/*
 	@GetMapping
 	public ResponseEntity<List<ClientDTO>> findAll() {
 		List<ClientDTO> list = service.findAll();			
 		return ResponseEntity.ok().body(list);
 	}
+	*/
 
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);			
+		return ResponseEntity.ok().body(list);
+	}
+
+	
 }
--- backend/src/main/java/com/devsuperior/dsclients/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclients/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsclients.dto.ClientDTO;
@@ -21,6 +23,11 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x-> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x->new ClientDTO(x));
 	}
 		
 	
