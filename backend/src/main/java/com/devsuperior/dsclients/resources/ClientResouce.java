549b7548f4febd661e52b70142042020af54fe62
Luiznt <luiznt@hotmail.com>
2021-05-18 16:35:01 -0300

    Find by id, insert, exceptions

--- backend/src/main/java/com/devsuperior/dsclients/resources/ClientResouce.java
+++ backend/src/main/java/com/devsuperior/dsclients/resources/ClientResouce.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsclients.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsclients.dto.ClientDTO;
 import com.devsuperior.dsclients.services.ClientService;
@@ -22,13 +27,13 @@ public class ClientResouce {
 	@Autowired
 	private ClientService service;
 	
-	/*
-	@GetMapping
+	
+	//@GetMapping
 	public ResponseEntity<List<ClientDTO>> findAll() {
 		List<ClientDTO> list = service.findAll();			
 		return ResponseEntity.ok().body(list);
 	}
-	*/
+	
 
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
@@ -43,5 +48,18 @@ public class ClientResouce {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert (@RequestBody ClientDTO dto ){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 	
 }
